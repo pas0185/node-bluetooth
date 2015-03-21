e2fb21540760c1d7523f6ea455b7aa46a98085ee
Patrick Sheehan <psheehan0301@gmail.com>
2015-03-21 18:12:54 -0500

    Added bare bones tableview controller

---
+++ Node+Bluetooth/NodeConnectionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NodeConnectionTableViewController.h
+//  Node+Bluetooth
+//
+//  Created by Patrick Sheehan on 3/21/15.
+//  Copyright (c) 2015 Patrick Sheehan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NodeConnectionTableViewController : UITableViewController
+
+@end
